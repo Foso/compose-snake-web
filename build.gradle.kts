b8afa5a994ef56181b1d48f029c5c1de4e5320fb
Jens Klingenberg <mail@jensklingenberg.de>
2021-09-04 09:09:59 +0200

    update to alpha03

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     kotlin("multiplatform") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha1"
+    id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
 repositories {
